e06ab3017aaf43069632cb79ea54a626805f96e1
CarolinaYoko <68036337+CarolinaYoko@users.noreply.github.com>
2020-08-05 22:23:43 -0300

    Editado

--- dos.js
+++ dos.js
@@ -10,7 +10,51 @@ Al finalizar, se debe informar:
 Considerar que las categorías pueden ser: almacen, lácteos, limpieza o bebidas y que el precio no puede ser menor a 0 ni mayor a 1000.
 */
 
-function mostrar()
-{
-	alert("Hola Mundo");
+function mostrar() { 
+	let NombreArt;
+	let Categoria;
+	let Precio;
+
+	NombreArt = prompt("Ingrese un artículo en minúscula ",);
+	Categoria = prompt("Ingrese una categoría en minúscula",);
+	Precio = prompt("Ingresar precio")
+
+	switch (Categoria) {
+		case "almacén":
+			switch (NombreArt) {
+				case "galletitas":
+					Precio = 70;
+					break;
+				case "pan":
+					Precio = 100;
+					break;
+				case "yerba":
+					Precio = 130;
+					break;
+			}
+			break;
+		case "lácteos":
+			switch (NombreArt) {
+				case "yogurt":
+					Precio = 100;
+					break;
+				case "leche":
+					Precio = 80;
+					break;
+			}
+			break;
+		case "limpieza":
+			switch (NombreArt) {
+				case "lavandina":
+					break;
+				case "jabón":
+					break;
+			}
+			break;
+
+		case "bebidas":
+			break;
+	}
+
+	
 }
